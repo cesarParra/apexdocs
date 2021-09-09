0719ea28cdb4ae68a895c411cf0ff7b3b2fe668f
Cesar Parra <cesar.parra@gmail.com>
2021-09-09 08:13:04 -0400

    Support for list types when generating JsDocs

--- src/JsHelper.ts
+++ src/JsHelper.ts
@@ -21,7 +21,19 @@ export default class JsHelper {
   }
 
   declareProperty(propertyType: string, propertyName: string, docDescription: string) {
-    const sanitizedType = this.sanitizeApexPropertyToJavascript(propertyType);
+    let sanitizedType;
+    // First we check if we are dealing with a list
+    if (this.isListProperty(propertyType)) {
+      // If we are then we first extract the type from the list
+      sanitizedType = this.extractTypeFromList(propertyType);
+      // then sanitize it
+      sanitizedType = this.sanitizeApexPropertyToJavascript(sanitizedType);
+      // and then we add brackets ([])
+      sanitizedType = sanitizedType + '[]';
+    } else {
+      sanitizedType = this.sanitizeApexPropertyToJavascript(propertyType);
+    }
+
     this.contents += ` * @property {${sanitizedType}} ${propertyName} ${docDescription}`;
     this.addBlankLine();
   }
@@ -31,6 +43,35 @@ export default class JsHelper {
     if (lowerPropertyType === 'decimal' || lowerPropertyType === 'integer' || lowerPropertyType === 'double') {
       return 'number';
     }
-    return lowerPropertyType;
+
+    if (lowerPropertyType === 'boolean' || lowerPropertyType === 'string') {
+      // If it is a built-in type then we return the lower-cased version.
+      return lowerPropertyType;
+    }
+
+    // Otherwise we respect casing
+    return propertyType;
+  }
+
+  isListProperty(propertyType: string) {
+    const lowerPropertyType = propertyType.toLowerCase();
+    if (lowerPropertyType.includes('<') && lowerPropertyType.includes('list')) {
+      return true;
+    }
+  }
+
+  extractTypeFromList(propertyType: string) {
+    const propertyWithoutList = this.replaceAll(propertyType, 'list', '');
+    return propertyWithoutList.replace('list', '')
+      .replace('<', '')
+      .replace('>', '');
+  }
+
+  // Case insensitive replace
+  replaceAll(sourceString: string, strReplace: string, strWith: string) {
+    // See http://stackoverflow.com/a/3561711/556609
+    const esc = strReplace.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    const reg = new RegExp(esc, 'ig');
+    return sourceString.replace(reg, strWith);
   }
 }
