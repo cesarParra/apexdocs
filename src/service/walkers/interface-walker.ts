a0242b845aeec0b79763d8a1baeb55d3b61c8171
Cesar Parra <cesar.parra@gmail.com>
2024-04-15 14:27:32 -0400

    Sorting interface methods as well.

--- src/service/walkers/interface-walker.ts
+++ src/service/walkers/interface-walker.ts
@@ -2,12 +2,11 @@ import { Walker, WalkerListener } from './walker';
 import { InterfaceMirror } from '@cparra/apex-reflection';
 
 export class InterfaceWalker extends Walker {
-  // TODO: Sort here as well
   walk(listener: WalkerListener): void {
     listener.onTypeDeclaration(this.type);
     const interfaceMirror = this.type as InterfaceMirror;
     if (interfaceMirror.methods.length) {
-      listener.onMethodsDeclaration(interfaceMirror.methods);
+      listener.onMethodsDeclaration(this.sortType(interfaceMirror.methods));
     }
   }
 }
