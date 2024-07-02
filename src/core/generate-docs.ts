228c7ac1494442968f9ff835d72ac323f5173ed2
cesarParra <cesar.parra@gmail.com>
2024-07-02 09:05:12 -0400

    Cleaning up

--- src/core/generate-docs.ts
+++ src/core/generate-docs.ts
@@ -1,31 +1,18 @@
 import { reflect, Type } from '@cparra/apex-reflection';
 import { typeToRenderableType } from '../adapters/apex-types';
 import { Renderable, RenderableEnum } from '../templating/types';
-import Handlebars from 'handlebars';
-import { typeDocPartial } from '../transpiler/markdown/plain-markdown/type-doc-partial';
-import { documentablePartialTemplate } from '../transpiler/markdown/plain-markdown/documentable-partial-template';
-import { methodsPartialTemplate } from '../transpiler/markdown/plain-markdown/methods-partial-template';
-import { constructorsPartialTemplate } from '../transpiler/markdown/plain-markdown/constructors-partial-template';
-import { fieldsPartialTemplate } from '../transpiler/markdown/plain-markdown/fieldsPartialTemplate';
 import { classMarkdownTemplate } from '../transpiler/markdown/plain-markdown/class-template';
 import { enumMarkdownTemplate } from '../transpiler/markdown/plain-markdown/enum-template';
 import { interfaceMarkdownTemplate } from '../transpiler/markdown/plain-markdown/interface-template';
-import { heading, heading2, heading3, inlineCode, splitAndCapitalize } from '../templating/helpers';
-import { link, resolveLinksInContent } from './markdown-helpers/resolve-links';
-import { convertCodeBlock } from './markdown-helpers/convert-code-block';
 import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
 import { flow } from 'fp-ts/function';
+import { CompilationRequest, Template } from './template';
 
 function doReflect(input: string): E.Either<string, Type> {
   const result = reflect(input);
   return result.error ? E.left(result.error.message) : E.right(result.typeMirror!);
 }
-
-type CompilationRequest = {
-  template: string;
-  source: Renderable;
-};
 
 function resolveTemplate(renderable: Renderable): CompilationRequest {
   return {
@@ -45,6 +32,12 @@ function getTemplate(renderable: Renderable): string {
   }
 }
 
+function compile(request: CompilationRequest): string {
+  return Template.getInstance().compile(request);
+}
+
+export const documentType = flow(typeToRenderableType, resolveTemplate, compile);
+
 export type DocOutput = {
   docContents: string;
   format: 'markdown';
@@ -52,8 +45,6 @@ export type DocOutput = {
   type: 'class' | 'interface' | 'enum';
   group: O.Option<string>;
 };
-
-export const documentType = flow(typeToRenderableType, resolveTemplate, compile);
 
 export function generateDocs(input: string): E.Either<string, DocOutput> {
   const result = doReflect(input);
@@ -68,32 +59,4 @@ export function generateDocs(input: string): E.Either<string, DocOutput> {
         group: O.fromNullable(type.group),
       }),
   )(result);
-}
-
-function compile(request: CompilationRequest): string {
-  Handlebars.registerPartial('typeDocumentation', typeDocPartial);
-  Handlebars.registerPartial('documentablePartialTemplate', documentablePartialTemplate);
-  Handlebars.registerPartial('methodsPartialTemplate', methodsPartialTemplate);
-  Handlebars.registerPartial('constructorsPartialTemplate', constructorsPartialTemplate);
-  Handlebars.registerPartial('fieldsPartialTemplate', fieldsPartialTemplate);
-  Handlebars.registerPartial('classTemplate', classMarkdownTemplate);
-  Handlebars.registerPartial('enumTemplate', enumMarkdownTemplate);
-  Handlebars.registerPartial('interfaceTemplate', interfaceMarkdownTemplate);
-
-  Handlebars.registerHelper('link', link);
-  Handlebars.registerHelper('code', convertCodeBlock);
-  Handlebars.registerHelper('withLinks', resolveLinksInContent);
-  Handlebars.registerHelper('heading', heading);
-  Handlebars.registerHelper('heading2', heading2);
-  Handlebars.registerHelper('heading3', heading3);
-  Handlebars.registerHelper('inlineCode', inlineCode);
-  Handlebars.registerHelper('splitAndCapitalize', splitAndCapitalize);
-
-  const compiled = Handlebars.compile(request.template);
-  return (
-    compiled(request.source)
-      .trim()
-      // clean up extra newlines
-      .replace(/\n{3,}/g, '\n\n')
-  );
 }
---
+++ src/core/template.ts
@@ -0,0 +1,59 @@
+import Handlebars from 'handlebars';
+import { typeDocPartial } from '../transpiler/markdown/plain-markdown/type-doc-partial';
+import { documentablePartialTemplate } from '../transpiler/markdown/plain-markdown/documentable-partial-template';
+import { methodsPartialTemplate } from '../transpiler/markdown/plain-markdown/methods-partial-template';
+import { constructorsPartialTemplate } from '../transpiler/markdown/plain-markdown/constructors-partial-template';
+import { fieldsPartialTemplate } from '../transpiler/markdown/plain-markdown/fieldsPartialTemplate';
+import { classMarkdownTemplate } from '../transpiler/markdown/plain-markdown/class-template';
+import { enumMarkdownTemplate } from '../transpiler/markdown/plain-markdown/enum-template';
+import { interfaceMarkdownTemplate } from '../transpiler/markdown/plain-markdown/interface-template';
+import { link, resolveLinksInContent } from './markdown-helpers/resolve-links';
+import { convertCodeBlock } from './markdown-helpers/convert-code-block';
+import { heading, heading2, heading3, inlineCode, splitAndCapitalize } from '../templating/helpers';
+import { Renderable } from '../templating/types';
+
+export type CompilationRequest = {
+  template: string;
+  source: Renderable;
+};
+
+export class Template {
+  private static instance: Template;
+
+  private constructor() {
+    Handlebars.registerPartial('typeDocumentation', typeDocPartial);
+    Handlebars.registerPartial('documentablePartialTemplate', documentablePartialTemplate);
+    Handlebars.registerPartial('methodsPartialTemplate', methodsPartialTemplate);
+    Handlebars.registerPartial('constructorsPartialTemplate', constructorsPartialTemplate);
+    Handlebars.registerPartial('fieldsPartialTemplate', fieldsPartialTemplate);
+    Handlebars.registerPartial('classTemplate', classMarkdownTemplate);
+    Handlebars.registerPartial('enumTemplate', enumMarkdownTemplate);
+    Handlebars.registerPartial('interfaceTemplate', interfaceMarkdownTemplate);
+
+    Handlebars.registerHelper('link', link);
+    Handlebars.registerHelper('code', convertCodeBlock);
+    Handlebars.registerHelper('withLinks', resolveLinksInContent);
+    Handlebars.registerHelper('heading', heading);
+    Handlebars.registerHelper('heading2', heading2);
+    Handlebars.registerHelper('heading3', heading3);
+    Handlebars.registerHelper('inlineCode', inlineCode);
+    Handlebars.registerHelper('splitAndCapitalize', splitAndCapitalize);
+  }
+
+  public static getInstance(): Template {
+    if (!Template.instance) {
+      Template.instance = new Template();
+    }
+    return Template.instance;
+  }
+
+  compile(request: CompilationRequest): string {
+    const compiled = Handlebars.compile(request.template);
+    return (
+      compiled(request.source)
+        .trim()
+        // clean up extra newlines
+        .replace(/\n{3,}/g, '\n\n')
+    );
+  }
+}
