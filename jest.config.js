1e7415ce5ac63dd16e978d2656fa46b0385f6dc4
cesarParra <cesar.parra@gmail.com>
2024-07-11 07:12:12 -0400

    Upgrading NPM modules

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/lib/'],
+  moduleNameMapper: {
+    '^chalk$': '<rootDir>/__mocks__/chalk.js',
+    '^log-update$': '<rootDir>/__mocks__/log-update.js',
+  },
 };
