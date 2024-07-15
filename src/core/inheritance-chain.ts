247b5e11b955ee9148ea8ddd0b6892ce6e7d2038
cesarParra <cesar.parra@gmail.com>
2024-07-15 08:07:24 -0400

    Working refactoring

--- src/core/inheritance-chain.ts
+++ src/core/inheritance-chain.ts
@@ -5,11 +5,19 @@ import * as O from 'fp-ts/Option';
 export function createInheritanceChain(repository: Type[], classMirror: ClassMirror): string[] {
   return pipe(
     O.fromNullable(classMirror.extended_class),
-    O.map((extendedClass) => repository.find((type) => type.name.toLowerCase() === extendedClass.toLowerCase())),
-    O.flatMap((type) => O.fromNullable(type as ClassMirror)),
     O.match(
-      () => (classMirror.extended_class ? [classMirror.extended_class] : []),
-      (extendedClass) => [classMirror.extended_class!, ...createInheritanceChain(repository, extendedClass)],
+      () => [],
+      (extendedClassName) => inheritanceChainFromParentClassName(repository, extendedClassName),
+    ),
+  );
+}
+
+function inheritanceChainFromParentClassName(repository: Type[], className: string): string[] {
+  return pipe(
+    O.fromNullable(repository.find((type) => type.name.toLowerCase() === className.toLowerCase())),
+    O.match(
+      () => [className],
+      (extendedClass: Type) => [className, ...createInheritanceChain(repository, extendedClass as ClassMirror)],
     ),
   );
 }
