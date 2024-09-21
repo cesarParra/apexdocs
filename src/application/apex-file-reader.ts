3593aca2f5e7185f5860acc6d1deee5b96637db4
cesarParra <cesar.parra@gmail.com>
2024-09-21 09:59:20 -0400

    Refactorings

--- src/application/apex-file-reader.ts
+++ src/application/apex-file-reader.ts
@@ -1,6 +1,8 @@
 import { FileSystem } from './file-system';
 import { UnparsedSourceFile } from '../core/shared/types';
 import { minimatch } from 'minimatch';
+import { pipe } from 'fp-ts/function';
+import { apply } from '#utils/fp';
 
 const APEX_FILE_EXTENSION = '.cls';
 
@@ -17,14 +19,14 @@ export class ApexFileReader {
     includeMetadata: boolean,
     exclude: string[],
   ): Promise<UnparsedSourceFile[]> {
-    // TODO: Make this more functional
-    // TODO: Unit tests
+    const processSingleFile = apply(this.processFile, fileSystem, includeMetadata);
 
-    const filePaths = await this.getFilePaths(fileSystem, rootPath);
-    const includedFilePaths = filePaths.filter((filePath) => !this.isExcluded(filePath, exclude));
-    const apexFilePaths = includedFilePaths.filter((filePath) => this.isApexFile(filePath));
-    const filePromises = apexFilePaths.map((filePath) => this.processFile(fileSystem, filePath, includeMetadata));
-    return Promise.all(filePromises);
+    return pipe(
+      await this.getFilePaths(fileSystem, rootPath),
+      (filePaths) => filePaths.filter((filePath) => !this.isExcluded(filePath, exclude)),
+      (filePaths) => filePaths.filter(this.isApexFile),
+      (filePaths) => Promise.all(filePaths.map(processSingleFile)),
+    );
   }
 
   private static async getFilePaths(fileSystem: FileSystem, rootPath: string): Promise<string[]> {
@@ -47,8 +49,8 @@ export class ApexFileReader {
 
   private static async processFile(
     fileSystem: FileSystem,
-    filePath: string,
     includeMetadata: boolean,
+    filePath: string,
   ): Promise<UnparsedSourceFile> {
     const rawTypeContent = await fileSystem.readFile(filePath);
     const metadataPath = `${filePath}-meta.xml`;
