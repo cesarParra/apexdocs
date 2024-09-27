255e500c05d3f4ed75679512c66d53685dfea5fa
cesarParra <cesar.parra@gmail.com>
2024-09-27 08:06:40 -0400

    Test - extracts arguments for openapi

--- src/cli/__tests__/args.spec.ts
+++ src/cli/__tests__/args.spec.ts
@@ -1,6 +1,6 @@
 import { extractArgs } from '../args';
 import { assertEither } from '../../core/test-helpers/assert-either';
-import { UserDefinedMarkdownConfig } from '../../core/shared/types';
+import { UserDefinedMarkdownConfig, UserDefinedOpenApiConfig } from '../../core/shared/types';
 
 describe('when extracting arguments', () => {
   describe('and no configuration is provided', () => {
@@ -17,6 +17,22 @@ describe('when extracting arguments', () => {
 
         const markdownConfig = configs[0] as UserDefinedMarkdownConfig;
         expect(markdownConfig.sourceDir).toEqual('force-app');
+      });
+    });
+
+    it('extracts the arguments from the process for the openapi command', async () => {
+      function getFromProcess() {
+        return ['openapi', '--sourceDir', 'force-app'];
+      }
+
+      const result = await extractArgs(getFromProcess);
+
+      assertEither(result, (configs) => {
+        expect(configs).toHaveLength(1);
+        expect(configs[0].targetGenerator).toEqual('openapi');
+
+        const openApiConfig = configs[0] as UserDefinedOpenApiConfig;
+        expect(openApiConfig.sourceDir).toEqual('force-app');
       });
     });
   });
