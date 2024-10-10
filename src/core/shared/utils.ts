8b405e9e92dfe46fe427d9024fa7f51327a13150
Cesar Parra <cesar.parra@gmail.com>
2024-10-10 08:12:05 -0400

    Cleaning up dist after every build (#193)
    * Cleaning up `dist` after every build
    
    * Cleaning up `dist` after every build

--- src/core/shared/utils.ts
+++ src/core/shared/utils.ts
@@ -1,5 +1,14 @@
 import { Skip } from './types';
 
+/**
+ * Represents a file to be skipped.
+ */
+export function skip(): Skip {
+  return {
+    _tag: 'Skip',
+  };
+}
+
 export function isSkip(value: unknown): value is Skip {
   return Object.prototype.hasOwnProperty.call(value, '_tag') && (value as Skip)._tag === 'Skip';
 }
