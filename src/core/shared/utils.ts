b32258801113980aeda602377a9b3ee4de770db0
cesarParra <cesar.parra@gmail.com>
2024-10-20 08:22:03 -0400

    Adding images to the README

--- src/core/shared/utils.ts
+++ src/core/shared/utils.ts
@@ -13,15 +13,6 @@ export function skip(): Skip {
   };
 }
 
-/**
- * Represents a file to be skipped.
- */
-export function skip(): Skip {
-  return {
-    _tag: 'Skip',
-  };
-}
-
 export function isSkip(value: unknown): value is Skip {
   return Object.prototype.hasOwnProperty.call(value, '_tag') && (value as Skip)._tag === 'Skip';
 }
