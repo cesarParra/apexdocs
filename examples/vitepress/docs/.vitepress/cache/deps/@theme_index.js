5a79777bf397198e0872b8ad4ccbd629b92b0519
Cesar Parra <cesar.parra@gmail.com>
2025-06-23 06:23:48 -0400

    Merge pull request #259
    * Upgrading to the latest version of `@salesforce/source-deploy-retrieve`

--- examples/vitepress/docs/.vitepress/cache/deps/@theme_index.js
+++ examples/vitepress/docs/.vitepress/cache/deps/@theme_index.js
@@ -9,33 +9,33 @@ import {
 } from "./chunk-574YRH25.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default6 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default7 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default9 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default10 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default11 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default12 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default13 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default14 } from "/home/cesar/projects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default6 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default7 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default9 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default10 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default11 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default12 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default13 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default14 } from "/Users/cesarparra/IdeaProjects/apexdocs/examples/vitepress/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
